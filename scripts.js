e71655208ad7c1e65fa9b4d50c59ba56f1aad595
Gerard Battaglia <erinbattaglia@Gerards-MacBook-Air.local>
2020-02-24 11:56:44 -0500

    added tip calculator

--- scripts.js
+++ scripts.js
@@ -1,7 +1,42 @@
-let userInput;
-userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
-if ( userInput === "burrito" ) {
-  $("#response").html("Brilliant choice!");
-} else {
-  $("#response").html("Don't you want a burrito?");
+/*let tipCalculator;
+tipCalculator = function(multiVariable, divVariable){
+let multiTotal;
+let divTotal;
+if (tipAmount < 1){
+multiTotal = total * tipAmount;
+$("#response").html("Your tip is $" + multiTotal);} else {
+divTotal = total % tipAmount;
+$("#response").html("Your tip is $" + divTotal);
 }
+};
+let tipAmount;
+tipAmount = 20;
+let total;
+total = 50;
+tipCalculator(); */
+/* let makeABurrito;
+makeABurrito = function(beansVariable){
+  let beansResponse;
+  beansResponse = "You ordered " + beansVariable + " beans. Good choice!";
+  $("#response").html(beansResponse);
+};
+let blackBeans;
+blackBeans = "black";
+makeABurrito(blackBeans); */
+let tipCalculator;
+tipCalculator = function(multiVariable, divVariable){
+  let multiTotal;
+  let divTotal;
+  if (tipAmount > 1){
+    multiTotal * tipAmount;
+    $("#response").html("Your tip is $" + multiTotal);}
+    else {
+      divTotal = total % tipAmount;
+      $("response").html("Your tip is $" + divTotal);
+    }
+};
+let tipAmount;
+tipAmount = 20;
+let total;
+total = 50;
+tipCalculator();
